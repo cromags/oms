4569c1fb00257efdc7b3cf265d97df4124243d26
cromags <hcpunq@wp.pl>
2020-09-15 16:08:38 +0100

    initial commit

---
+++ src/test/java/pl/bw/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bw.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
